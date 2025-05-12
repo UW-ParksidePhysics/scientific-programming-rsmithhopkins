03e3775b784a657d680fcb7ab66cc61b68e5265e
rsmithhopkins <smith518@rangers.uwp.edu>
2025-05-12 17:28:01 -0500

    Matrices

--- python/matrices/fifth_eigenvector.py
+++ python/matrices/fifth_eigenvector.py
@@ -1,1 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create a 5x5 matrix using diagflat and ones functions
+matrix = np.diagflat(np.ones(5))
+eigenvalues, eigenvectors = np.linalg.eig(matrix)
+fifth_eigenvector = eigenvectors[:, 4]
+x_values = np.linspace(0, 1, 5)
+
+# Define the function sqrt(2) * sin(pi * x)
+function_values = np.sqrt(2) * np.sin(np.pi * x_values)
+
+plt.plot(x_values, fifth_eigenvector, label='Fifth Eigenvector')
+
+# Plot the function sqrt(2) * sin(pi * x) for comparison
+plt.plot(x_values, function_values, label='sqrt(2) * sin(pi * x)')
+plt.xlabel('x')
+plt.ylabel('Value')
+plt.legend()
+
+# Just turning the plot into an actual image
+plt.savefig('fifth_eigenvector_plot.png')
+plt.show()
 
--- python/matrices/tenth_eigenvector.py
+++ python/matrices/tenth_eigenvector.py
@@ -1,1 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Set the matrix dimension
+matrix_dimension = 10
+
+matrix = np.diagflat(np.ones(matrix_dimension))
+eigenvalues, eigenvectors = np.linalg.eig(matrix)
+tenth_eigenvector = eigenvectors[:, 9]
+x_values = np.linspace(0, 1, matrix_dimension)
+
+# Define the function sqrt(2) * sin(pi * x)
+function_values = np.sqrt(2) * np.sin(np.pi * x_values)
+
+plt.plot(x_values, tenth_eigenvector, label='Tenth Eigenvector')
+
+# Plot the function sqrt(2) * sin(pi * x) for comparison
+plt.plot(x_values, function_values, label='sqrt(2) * sin(pi * x)')
+plt.xlabel('x')
+plt.ylabel('Value')
+plt.legend()
+
+# Just turning the plot into an actual image
+plt.savefig('tenth_eigenvector_plot.png')
+plt.show()
