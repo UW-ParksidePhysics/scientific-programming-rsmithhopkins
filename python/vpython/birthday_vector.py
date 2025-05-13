5c6b89c9bf9e403a41c91097fe4aec160eb9d87e
rsmithhopkins <smith518@rangers.uwp.edu>
2025-05-12 22:04:32 -0500

    Command Line Assignments and VPython

--- python/vpython/birthday_vector.py
+++ python/vpython/birthday_vector.py
@@ -1,12 +1,10 @@
-from vpython import *
-# GlowScript 3.0 VPython
+from vpython import scene, cylinder, label, arrow, vector, color  # Required for PyCharm
 
+# GlowScript 3.0 VPython
 # Written by Ruth Chabay, licensed under Creative Commons 4.0.
 # All uses permitted, but you must not claim that you wrote it, and
 # you must include this license information in any copies you make.
 # For details see http://creativecommons.org/licenses/by/4.0
-
-from vpython import scene, cylinder, label, arrow, vector, color  # Import necessary VPython modules
 
 scene.background = color.white
 scene.width = 600
@@ -19,7 +17,6 @@ On a two-button mouse, middle is left + right.
 To pan left/right and up/down, Shift-drag.
 Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
 
-# Draw axes
 x_axis = cylinder(color=vector(1, 0, 0), pos=vector(0, 0, 0), axis=vector(10, 0, 0), radius=0.3)
 x_label = label(pos=vector(11, 0, 0), text="x", color=color.red, opacity=0, height=30, box=False)
 
@@ -30,8 +27,9 @@ z_axis = cylinder(color=vector(0, 0, 1), pos=vector(0, 0, 0), axis=vector(0, 0, 
 z_label = label(pos=vector(0, 0, 11), text="z", color=color.blue, opacity=0, height=30, box=False)
 
 # Create a vector in the first octant
-# Numbers come from birth dates and their months: 7 (July), 12 (December), and 9 (September)
-custom_vector = arrow(pos=vector(0, 0, 0), axis=vector(7, 12, 9), color=color.orange, shaftwidth=0.5)
+# Numbers chosen from pair members' birthdates: 6 (June), 11 (November), 8 (August)
+r_vector = arrow(pos=vector(0, 0, 0), axis=vector(6, 11, 8), color=color.cyan, shaftwidth=0.5)
 
-# Label the vector as r = ax + by + cz
-vector_label = label(pos=vector(8, 13, 10), text="r = 7x + 12y + 9z", color=color.orange, height=20, box=False)
+# Label the vector as r = ai + bj + ck
+r_label = label(pos=vector(7, 12, 9), text="r = 6i + 11j + 8k", color=color.cyan, height=20, box=False)
+
